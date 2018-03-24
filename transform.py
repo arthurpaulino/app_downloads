eb49b0823cc5106e4d37323afaa10e208c3e41ad
Arthur Paulino <arthurleonardo.ap@gmail.com>
2018-03-24 13:30:26 -0300

    cutting features off

--- transform.py
+++ transform.py
@@ -19,18 +19,23 @@ def get_intervals_std(obj):
     return std
 
 def generate_attributed_perc_features(df, groupbys):
+    print('# attributed perc-related features')
     for groupby in groupbys:
+        print('## group by {}'.format(groupby))
         prefix = '_'.join(groupby)+'_'
 
         start = time.time()
         attributed_count = df.groupby(groupby)['is_attributed'].transform(get_attributed_count)
         df[prefix+'attributed_perc'] = attributed_count/df[prefix+'clicks']
+        df.drop(columns=[prefix+'clicks'], inplace=True)
         del attributed_count
         gc.collect()
         print('{:.2f}s to generate feature {}'.format(time.time()-start, prefix+'attributed_perc'))
 
 def generate_time_related_features(df, groupbys):
+    print('# time-related features')
     for groupby in groupbys:
+        print('## group by {}'.format(groupby))
         prefix = '_'.join(groupby)+'_'
 
         start = time.time()
@@ -48,7 +53,7 @@ def generate_time_related_features(df, groupbys):
         gc.collect()
         print('{:.2f}s to generate feature {}'.format(time.time()-start, prefix+'clicks_frequency'))
 
-        if prefix != 'app_':
+        if len(groupby)>1 or (len(groupby)==1 and groupby[0]=='ip'):
             start = time.time()
             df[prefix+'duration_on_app'] = df.groupby(groupby+['app'])['click_time'].transform(get_timediff).astype('uint32')
             gc.collect()
@@ -70,7 +75,9 @@ def generate_time_related_features(df, groupbys):
             print('{:.2f}s to generate feature {}'.format(time.time()-start, prefix+'clicks_on_app_frequency'))
 
 def generate_count_related_features(df, groupbys):
+    print('# count-related features')
     for groupby in groupbys:
+        print('## group by {}'.format(groupby))
         prefix = '_'.join(groupby)+'_'
 
         start = time.time()
@@ -78,7 +85,7 @@ def generate_count_related_features(df, groupbys):
         gc.collect()
         print('{:.2f}s to generate feature {}'.format(time.time()-start, prefix+'clicks'))
 
-        if prefix != 'app_':
+        if len(groupby)>1 or (len(groupby)==1 and groupby[0]=='ip'):
             start = time.time()
             df[prefix+'clicks_on_app'] = df.groupby(groupby+['app'])['ip'].transform('count').astype('uint32')
             gc.collect()
@@ -90,17 +97,18 @@ def generate_count_related_features(df, groupbys):
             print('{:.2f}s to generate feature {}'.format(time.time()-start, prefix+'clicks_on_app_perc'))
 
 def transform(df):
-    groupbys = [['ip'], ['app'], ['os'], ['device'], ['channel'], ['ip', 'os'], ['ip', 'os', 'device'], ['ip', 'os', 'device', 'channel']]
+    groupbys = [['ip'], ['app'], ['os'], ['channel'], ['ip', 'os']]
     generate_count_related_features(df, groupbys)
+    
+    groupbys = [['ip']]
+    #groupbys = [['ip'], ['ip', 'os']]
     generate_time_related_features(df, groupbys)
 
     df.drop(columns=['ip'], inplace=True)
 
-    groupbys = [['app'], ['os'], ['device'], ['channel']]
+    groupbys = [['app'], ['os'], ['channel']]
     generate_attributed_perc_features(df, groupbys)
 
     df['click_time'] = df['click_time'].apply(lambda x: x.hour).astype('uint8')
-
-    df.drop(columns=['os', 'device', 'app', 'channel'], inplace=True)
 
     print('new dataframe shape: {}'.format(df.shape))
