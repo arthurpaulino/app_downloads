10814b53147bfd34585c1f9ca4d3de632f7b6e3b
Arthur Paulino <arthurleonardo.ap@gmail.com>
2018-03-25 21:52:33 -0300

    sorting base features

--- transform.py
+++ transform.py
@@ -3,6 +3,42 @@ import numpy as np
 import time
 import gc
 
+def sort_features_by_attr_proba(df, features):
+    for feature in features:
+        print('--- sorting {}'.format(feature))
+        start = time.time()
+
+        def compute_score(feature_id):
+            attrs = df[(df[feature]==feature_id) & (df['is_attributed']==1)]
+            n_attrs = attrs.shape[0]
+            del attrs
+            gc.collect()
+
+            clicks = df[df[feature]==feature_id]
+            n_clicks = clicks.shape[0]
+            del clicks
+            gc.collect()
+
+            if n_attrs==0:
+                return -n_clicks
+            return n_attrs/n_clicks
+
+        scores = {}
+        feature_ids = set(df[feature])
+        for feature_id in feature_ids:
+            scores[feature_id] = compute_score(feature_id)
+        sorted_ids = sorted(feature_ids, key=lambda feature_id: scores[feature_id], reverse=True)
+        del scores
+        gc.collect()
+
+        sequenced_ids = [i+1 for i in range(len(sorted_ids))]
+
+        df[feature].replace(sorted_ids, sequenced_ids, inplace=True)
+        del sorted_ids, sequenced_ids
+        gc.collect()
+
+        print('{:.2f}s to sort {}'.format(time.time()-start, feature))
+
 def generate_count_features(df, groupbys):
     for groupby in groupbys:
         print('--- grouping by {}'.format(groupby))
@@ -14,6 +50,9 @@ def generate_count_features(df, groupbys):
         print('{:.2f}s to generate feature {}'.format(time.time()-start, prefix+'clicks'))
 
 def transform(df):
+
+    sort_features_by_attr_proba(df, ['app', 'os', 'channel'])
+
     start = time.time()
     df['hour'] = pd.to_datetime(df['click_time']).dt.hour.astype('uint8')
     df.drop(columns=['click_time'], inplace=True)
